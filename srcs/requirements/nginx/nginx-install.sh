c641b33a4e7ce9260030a81bebc03812543fde2b
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2026-03-28 20:36:36 +0100

    add docker-compose first code, and created dockerfile for nginx (not tested yet)

---
+++ srcs/requirements/nginx/nginx-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    nginx-install.sh                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: meghribe <meghribe@student.42barcelona.co  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/28 20:33:36 by meghribe          #+#    #+#              #
+#    Updated: 2026/03/28 20:33:36 by meghribe         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Note: this script does not verify that the downloaded file container the proper key
+# tooo much for me
+# font: https://nginx.org/en/linux_packages.html#Alpine
+
+echo ' installing the prerequisites'
+sudo apk add openssl curl ca-certificates
+
+echo ' setting up the apk repository for stable nginx packages '
+printf "%s%s%s%s\n" \
+    "@nginx " \
+    "https://nginx.org/packages/alpine/v" \
+    `egrep -o '^[0-9]+\.[0-9]+' /etc/alpine-release` \
+    "/main" \
+    | sudo tee -a /etc/apk/repositories
+
+echo ' fetching the original signing key '
+curl -o /tmp/nginx_signing.rsa.pub https://nginx.org/keys/nginx_signing.rsa.pub
+
+echo ' moving key to apk trusted keys storage '
+sudo mv /tmp/nginx_signing.rsa.pub /etc/apk/keys/
+
+echo ' installing nginx '
+sudo apk add nginx@nginx

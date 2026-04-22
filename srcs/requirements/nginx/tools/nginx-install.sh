2126d29e21721ddb5a56204d471c86115354ed6d
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2026-04-22 11:30:29 +0200

    change from alpine to debian

--- srcs/requirements/nginx/tools/nginx-install.sh
+++ srcs/requirements/nginx/tools/nginx-install.sh
@@ -12,27 +12,8 @@
 # **************************************************************************** #
 
 # Note: this script does not verify that the downloaded file container the proper key
-# tooo much for me
-# font: https://nginx.org/en/linux_packages.html#Alpine
+# tooo much for me :P
+# font: https://nginx.org/en/linux_packages.html#Debian
 
-echo ' installing the prerequisites'
-apk add openssl curl ca-certificates
-
-echo ' setting up the apk repository for stable nginx packages '
-printf "%s%s%s%s\n" \
-    "@nginx " \
-    "https://nginx.org/packages/alpine/v" \
-    `egrep -o '^[0-9]+\.[0-9]+' /etc/alpine-release` \
-    "/main" \
-    | tee -a /etc/apk/repositories
-
-echo ' fetching the original signing key '
-curl -o /tmp/nginx_signing.rsa.pub https://nginx.org/keys/nginx_signing.rsa.pub
-
-echo ' moving key to apk trusted keys storage '
-mv /tmp/nginx_signing.rsa.pub /etc/apk/keys/
-
-echo ' installing nginx '
-apk add nginx@nginx
-
-#exec "/usr/sbin/nginx -g daemon off"
+apt update
+apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring nginx -y

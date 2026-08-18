bf70c72cea58887d9d76455057d6d178b3289531
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2026-08-18 02:48:58 +0200

    add from debian latest (later change it)

--- srcs/requirements/wordpress/tools/download-wordpress.sh
+++ srcs/requirements/wordpress/tools/download-wordpress.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt update
-apt install wget -y
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
+apt install wget php -y
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
